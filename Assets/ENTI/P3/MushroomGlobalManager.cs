a7bd323f77b6aa4ec5cdff1ee202551bf275ef49
Aleix <aleix.martinez@enti.cat>
2026-05-20 01:51:56 +0200

    Setas Def

--- Assets/ENTI/P3/MushroomGlobalManager.cs
+++ Assets/ENTI/P3/MushroomGlobalManager.cs
@@ -1,20 +1,19 @@
 using UnityEngine;
 
+[ExecuteAlways]
 public class MushroomGlobalManager : MonoBehaviour
 {
-    [Header("Jugador")]
     public Transform playerTransform;
 
     [Header("Ajustes Globales")]
-    public float globalAffectRadius = 1.18f;
-    public float globalAffectContrast = 0f;
-    public float globalAffectIntensity = 0.5f;
+    public float globalAffectRadius = 5f;
+    public float globalAffectContrast = 1f;
+    public float globalAffectIntensity = 1f;
 
     void Update()
     {
         if (playerTransform == null) return;
 
-        // Ahora los nombres coinciden EXACTAMENTE con tu código HLSL
         Shader.SetGlobalVector("_PlayerPosition", playerTransform.position);
         Shader.SetGlobalFloat("_Affect_Radius", globalAffectRadius);
         Shader.SetGlobalFloat("_Affect_Contrast", globalAffectContrast);
--- Assets/ENTI/P3/MushroomInstanceColor.cs
+++ Assets/ENTI/P3/MushroomInstanceColor.cs
@@ -7,20 +7,19 @@ public class MushroomInstanceColor : MonoBehaviour
         Renderer rend = GetComponent<Renderer>();
         if (rend == null) return;
 
-        // Creamos un color aleatorio (tipo neón/vibrante para que se vea bien)
+      
         Color randomColor = Color.HSVToRGB(Random.value, 0.8f, 1f);
 
-        // Creamos el PropertyBlock. Esto modifica las variables del shader
-        // SOLO para este objeto, sin crear un material nuevo.
+       
         MaterialPropertyBlock propBlock = new MaterialPropertyBlock();
 
-        // Leemos si el objeto ya tiene otras propiedades
+      
         rend.GetPropertyBlock(propBlock);
 
-        // Le inyectamos el color. OJO: "_Color" debe ser el Reference exacto de tu Shader Graph
+       
         propBlock.SetColor("_Color", randomColor);
 
-        // Se lo devolvemos al renderizador
+        
         rend.SetPropertyBlock(propBlock);
     }
 }
